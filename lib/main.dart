2df47a3e820fa0c655c967f2daada1c241bfbfc3
Adam Yoneda <adam.yoneda.developer@gmail.com>
2025-04-01 19:35:40 +0900

    add: flutter_datetime_picker_plus

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart'
+    as picker;
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const MyApp());
+
+class CustomPicker extends picker.CommonPickerModel {
+  String digits(int value, int length) {
+    return '$value'.padLeft(length, "0");
+  }
+
+  CustomPicker({DateTime? currentTime, super.locale}) {
+    this.currentTime = currentTime ?? DateTime.now();
+    setLeftIndex(this.currentTime.hour);
+    setMiddleIndex(this.currentTime.minute);
+    setRightIndex(this.currentTime.second);
+  }
+
+  @override
+  String? leftStringAtIndex(int index) {
+    if (index >= 0 && index < 24) {
+      return digits(index, 2);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  String? middleStringAtIndex(int index) {
+    if (index >= 0 && index < 60) {
+      return digits(index, 2);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  String? rightStringAtIndex(int index) {
+    if (index >= 0 && index < 60) {
+      return digits(index, 2);
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  String leftDivider() {
+    return "|";
+  }
+
+  @override
+  String rightDivider() {
+    return "|";
+  }
+
+  @override
+  List<int> layoutProportions() {
+    return [1, 2, 1];
+  }
+
+  @override
+  DateTime finalTime() {
+    return currentTime.isUtc
+        ? DateTime.utc(currentTime.year, currentTime.month, currentTime.day,
+            currentLeftIndex(), currentMiddleIndex(), currentRightIndex())
+        : DateTime(currentTime.year, currentTime.month, currentTime.day,
+            currentLeftIndex(), currentMiddleIndex(), currentRightIndex());
+  }
 }
 
 class MyApp extends StatelessWidget {
@@ -13,113 +77,176 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Datetime Picker'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            TextButton(
+                onPressed: () {
+                  picker.DatePicker.showDatePicker(context,
+                      showTitleActions: true,
+                      minTime: DateTime(2018, 3, 5),
+                      maxTime: DateTime(2019, 6, 7),
+                      theme: const picker.DatePickerTheme(
+                          headerColor: Colors.orange,
+                          backgroundColor: Colors.blue,
+                          itemStyle: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                          doneStyle:
+                              TextStyle(color: Colors.white, fontSize: 16)),
+                      onChanged: (date) {
+                    print(
+                        'change $date in time zone ${date.timeZoneOffset.inHours}');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, currentTime: DateTime.now(), locale: picker.LocaleType.en);
+                },
+                child: const Text(
+                  'show date picker(custom theme &date time range)',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            TextButton(
+                onPressed: () {
+                  picker.DatePicker.showTimePicker(context,
+                      showTitleActions: true, onChanged: (date) {
+                    print(
+                        'change $date in time zone ${date.timeZoneOffset.inHours}');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, currentTime: DateTime.now());
+                },
+                child: const Text(
+                  'show time picker',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            TextButton(
+                onPressed: () {
+                  picker.DatePicker.showTime12hPicker(context,
+                      showTitleActions: true, onChanged: (date) {
+                    print(
+                        'change $date in time zone ${date.timeZoneOffset.inHours}');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, currentTime: DateTime.now());
+                },
+                child: const Text(
+                  'show 12H time picker with AM/PM',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            TextButton(
+                onPressed: () {
+                  picker.DatePicker.showDateTimePicker(context,
+                      showTitleActions: true,
+                      minTime: DateTime(2020, 5, 5, 20, 50),
+                      maxTime: DateTime(2020, 6, 7, 05, 09), onChanged: (date) {
+                    print(
+                        'change $date in time zone ${date.timeZoneOffset.inHours}');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, locale: picker.LocaleType.zh);
+                },
+                child: const Text(
+                  'show date time picker (Chinese)',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            TextButton(
+                onPressed: () {
+                  picker.DatePicker.showDateTimePicker(context,
+                      showTitleActions: true, onChanged: (date) {
+                    print(
+                        'change $date in time zone ${date.timeZoneOffset.inHours}');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  }, currentTime: DateTime(2008, 12, 31, 23, 12, 34));
+                },
+                child: const Text(
+                  'show date time picker (English-America)',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            TextButton(
+                onPressed: () {
+                  picker.DatePicker.showDateTimePicker(context,
+                      showTitleActions: true, onChanged: (date) {
+                    print(
+                        'change $date in time zone ${date.timeZoneOffset.inHours}');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  },
+                      currentTime: DateTime(2008, 12, 31, 23, 12, 34),
+                      locale: picker.LocaleType.nl);
+                },
+                child: const Text(
+                  'show date time picker (Dutch)',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            TextButton(
+                onPressed: () {
+                  picker.DatePicker.showDateTimePicker(context,
+                      showTitleActions: true, onChanged: (date) {
+                    print(
+                        'change $date in time zone ${date.timeZoneOffset.inHours}');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  },
+                      currentTime: DateTime(2008, 12, 31, 23, 12, 34),
+                      locale: picker.LocaleType.ru);
+                },
+                child: const Text(
+                  'show date time picker (Russian)',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            TextButton(
+                onPressed: () {
+                  picker.DatePicker.showDateTimePicker(context,
+                      showTitleActions: true, onChanged: (date) {
+                    print(
+                        'change $date in time zone ${date.timeZoneOffset.inHours}');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  },
+                      currentTime: DateTime.utc(2019, 12, 31, 23, 12, 34),
+                      locale: picker.LocaleType.de);
+                },
+                child: const Text(
+                  'show date time picker in UTC (German)',
+                  style: TextStyle(color: Colors.blue),
+                )),
+            TextButton(
+                onPressed: () {
+                  picker.DatePicker.showPicker(context, showTitleActions: true,
+                      onChanged: (date) {
+                    print(
+                        'change $date in time zone ${date.timeZoneOffset.inHours}');
+                  }, onConfirm: (date) {
+                    print('confirm $date');
+                  },
+                      pickerModel: CustomPicker(currentTime: DateTime.now()),
+                      locale: picker.LocaleType.en);
+                },
+                child: const Text(
+                  'show custom time picker,\nyou can custom picker model like this',
+                  style: TextStyle(color: Colors.blue),
+                )),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
